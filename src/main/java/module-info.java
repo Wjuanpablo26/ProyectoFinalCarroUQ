59871a058aaa63bb6162320d9088617f1763f507
Wjuanpablo26 <juanp.reyg@uqvirtual.edu.co>
2024-11-11 21:29:51 -0500

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.poo.proyectofinalcarrouq {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.poo.proyectofinalcarrouq to javafx.fxml;
+    exports co.edu.uniquindio.poo.proyectofinalcarrouq;
+}
