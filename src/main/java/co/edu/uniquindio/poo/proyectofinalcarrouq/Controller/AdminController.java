3aec87bde7e8f092041f6614faef40a8ac533700
Camila Mejia. <mariac.mejiap@uqvirtual.edu.co>
2024-11-16 20:17:47 -0500

    AdminController completo

---
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalcarrouq/Controller/AdminController.java
@@ -0,0 +1,51 @@
+package co.edu.uniquindio.poo.proyectofinalcarrouq.Controller;
+
+public class AdminController {
+
+    ModelFactory modelFactory;
+
+    public AdminController() {
+        modelFactory = ModelFactory.getInstance();
+    }
+
+    //Empleado
+    public int addEmpleado(Empleado empleado) throws PersonaException {
+        return modelFactory.addEmpleado(empleado);
+    }
+
+    public int removeEmpleado(Empleado empleado) throws PersonaException {
+        return modelFactory.deleteEmpleado(empleado);
+    }
+
+    public boolean actualizarEmpleado(String cedula,Empleado empleado) throws PersonaException {
+        return modelFactory.actualizarEmpleado(cedula, empleado);
+    }
+
+    public ArrayList<Empleado> obtenerEmpleados() {
+        return modelFactory.obtenerEmpleados();
+    }
+
+    //Admin
+    public int addAdmin(Admin admin) throws PersonaException {
+        return modelFactory.addAdmin(admin);
+    }
+
+    public int removeAdmin(Admin admin) throws PersonaException {
+        return modelFactory.deleteAdmin(admin);
+    }
+
+    public boolean actualizarAdmin(String cedula,Admin admin) throws PersonaException {
+        return modelFactory.actualizarAdmin(cedula, admin);
+    }
+
+    public ArrayList<Admin> obtenerAdmins() {
+        return modelFactory.obtenerAdmins();
+    }
+
+    //Ventas
+    public ArrayList<Venta> obtenerVentasEmpleado(String cedula){
+        return modelFactory.obtenerVentasEmpleado(cedula);
+    }
+
+
+}
