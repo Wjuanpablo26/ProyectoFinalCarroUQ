6cf13b5cba430462bf9f9a9e444bc80a8b56f860
juan jose <juanj.suarezm@uqvirtual.edu.co>
2024-11-17 23:18:25 -0500

    agregacion de metodos a crudVehiculo

--- src/main/java/co/edu/uniquindio/poo/proyectofinalcarrouq/View/CrudVehiculo.java
+++ src/main/java/co/edu/uniquindio/poo/proyectofinalcarrouq/View/CrudVehiculo.java
@@ -5,6 +5,7 @@ import co.edu.uniquindio.poo.proyectofinalcarrouq.Model.Enum.TipoCamiones;
 import co.edu.uniquindio.poo.proyectofinalcarrouq.Model.Enum.TipoCombustible;
 import co.edu.uniquindio.poo.proyectofinalcarrouq.Model.Enum.TipoEstado;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
@@ -270,5 +271,208 @@ public class CrudVehiculo {
     private TextField txtVelocidad;
 
 
-    
+        private void limpiarCampos(){
+        // Limpiar todos los campos de texto
+        txtMarca.clear();
+        txtModelo.clear();
+        txtCilindraje.clear();
+        txtVelocidad.clear();
+        txtNumPasajeros.clear();
+        txtPuertas.clear();
+        txtCapacidadCarga.clear();
+        txtCapacidadCarga.clear();
+        txtNumEjes.clear();
+        txtNumBolsasAire.clear();
+        txtBolsaAireDep.clear();
+        txtCaballosFuerzaDep.clear();
+        txtTiempAceleDep.clear();
+        txtCapCargaCam.clear();
+        txtCargaRapida.clear();
+        txtDuracionBateria.clear();
+        txtNumEjesCam.clear();
+        txtCapacidadCarga.clear();
+        txtPlaca.clear();
+
+        // Restablecer la sleccion en todos los comboBox
+        cbxCambio.setValue(null);
+        cbxCamiones.setValue(null);
+        cbxCombustible.setValue(null);
+        cbxTipoVehiculo.setValue(null);
+
+        // Restablecer la seleccion en todos los ToggleGroups (RadioButtons)
+        tieneABS.selectToggle(null);
+        tieneAireAcon.selectToggle(null);
+        tieneCamara.selectToggle(null);
+        tieneVelocidad.selectToggle(null);
+        tieneSensorColi.selectToggle(null);
+        tieneSensorTraf.selectToggle(null);
+        tieneAsistente.selectToggle(null);
+        tiene4x4.selectToggle(null);
+        tieneAerodinamicaDep.selectToggle(null);
+        tieneFrenosAire.selectToggle(null);
+        esNuevo.selectToggle(null);
+        esEnchufable.selectToggle(null);
+        esHibridoLigero.selectToggle(null);
+
+        // Limpiar imagen seleccionada en ImageView, si es necesario
+        imagenView.setImage(null);
+        txtPlaca.setEditable(true);
+        cbxTipoVehiculo.setDisable(false);
+        imagenSeleccionada = null;
+
+
+        }
+
+    private boolean verificarCampos(){
+        String tipoVehiculo = cbxTipoVehiculo.getValue();
+        boolean camposCompletos = true;
+
+        camposCompletos &= !txtPlaca.getText().isEmpty();
+        camposCompletos &= !txtMarca.getText().isEmpty();
+        camposCompletos &= esNuevo.getSelectedToggle() != null;
+        camposCompletos &= !txtModelo.getText().isEmpty();
+        camposCompletos &= cbxCambio.getValue() != null;
+        camposCompletos &= !txtVelocidad.getText().isEmpty();
+        camposCompletos &= !txtCilindraje.getText().isEmpty();
+        camposCompletos &= !cbxCombustible.getValue() != null;
+
+        // Verificar campos dependiendo del tipo de vehiculo
+        switch (tipoVehiculo) {
+            case "Motos":
+                break;
+            
+            case "Sedan":
+                camposCompletos &= !txtNumPasajeros.getText().isEmpty();
+                camposCompletos &= !txtPuertas.getText().isEmpty();
+                camposCompletos &= !txtCapacidadMaletero.getText().isEmpty();
+                camposCompletos &= tieneAireAcon.getSelectedToggle() != null;
+                camposCompletos &= tieneCamara.getSelectedToggle() != null;
+                camposCompletos &= tieneVelocidad.getSelectedToggle() != null;
+                camposCompletos &= !txtNumBolsasAire.getText().isEmpty();
+                camposCompletos &= tieneABS.getSelectedToggle() != null;
+                camposCompletos &= tieneSensorColi.getSelectedToggle() != null;
+                camposCompletos &= tieneSensorTraf.getSelectedToggle() != null;
+                camposCompletos &= tieneAsistente.getSelectedToggle() != null;
+                break;
+            
+            case "Deportivo":
+                camposCompletos &= !txtMarca.getText().isEmpty();
+                camposCompletos &= esNuevo.getSelectedToggle() != null;
+                camposCompletos &= !txtModelo.getText().isEmpty();
+                camposCompletos &= cbxCambio.getValue() != null;
+                camposCompletos &= !txtVelocidad.getText().isEmpty();
+                camposCompletos &= !txtCilindraje.getText().isEmpty();
+                camposCompletos &= !txtNumPasajeros.getText().isEmpty();
+                camposCompletos &= !txtPuertas.getText().isEmpty();
+                camposCompletos &= !txtBolsaAireDep.getText().isEmpty();
+                camposCompletos &= !txtCaballosFuerzaDep.getText().isEmpty();
+                camposCompletos &= !txtTiempAceleDep.getText().isEmpty();
+                break;
+
+            case "Camionetas":
+                camposCompletos &= !txtMarca.getText().isEmpty();
+                camposCompletos &= esNuevo.getSelectedToggle() != null;
+                camposCompletos &= !txtModelo.getText().isEmpty();
+                camposCompletos &= cbxCambio.getValue() != null;
+                camposCompletos &= !txtVelocidad.getText().isEmpty();
+                camposCompletos &= !txtCilindraje.getText().isEmpty();
+                camposCompletos &= !txtNumPasajeros.getText().isEmpty();
+                camposCompletos &= !txtPuertas.getText().isEmpty();
+                camposCompletos &= !txtCapacidadMaletero.getText().isEmpty();
+                camposCompletos &= tieneAireAcon.getSelectedToggle() != null;
+                camposCompletos &= tieneCamara.getSelectedToggle() != null;
+                camposCompletos &= tieneVelocidad.getSelectedToggle() != null;
+                camposCompletos &= !txtNumBolsasAire.getText().isEmpty();
+                camposCompletos &= tieneABS.getSelectedToggle() != null;
+                camposCompletos &= tieneSensorColi.getSelectedToggle() != null;
+                camposCompletos &= tieneSensorTraf.getSelectedToggle() != null;
+                camposCompletos &= tieneAsistente.getSelectedToggle() != null;
+                camposCompletos &= tiene4x4.getSelectedToggle() != null;
+                break;
+
+            case "PickUps":
+                camposCompletos &=  !txtNumPasajeros.getText().isEmpty();
+                camposCompletos &= !txtPuertas.getText().isEmpty();
+                camposCompletos &= tieneAireAcon.getSelectedToggle() != null;
+                camposCompletos &= tieneCamara.getSelectedToggle() != null;
+                camposCompletos &= !txtNumBolsasAire.getText().isEmpty();
+                camposCompletos &= tieneABS.getSelectedToggle() != null;
+                camposCompletos &= tiene4x4.getSelectedToggle() != null;
+                camposCompletos &= !txtCapacidadCarga.getText().isEmpty();
+                break;
+
+            case "vans":
+                camposCompletos &= !txtMarca.getText().isEmpty();
+                camposCompletos &= esNuevo.getSelectedToggle() != null;
+                camposCompletos &= !txtModelo.getText().isEmpty();
+                camposCompletos &= cbxCambio.getValue() != null;
+                camposCompletos &= !txtVelocidad.getText().isEmpty();
+                camposCompletos &= !txtVelocidad.getText().isEmpty();
+                camposCompletos &= !txtCilindraje.getText().isEmpty();
+                camposCompletos &= !txtNumPasajeros.getText().isEmpty();
+                camposCompletos &= !txtPuertas.getText().isEmpty();
+                camposCompletos &= !txtCapacidadMaletero.getText().isEmpty();
+                camposCompletos &= tieneAireAcon.getSelectedToggle() != null;
+                camposCompletos &= tieneCamara.getSelectedToggle() != null;
+                camposCompletos &= !txtNumBolsasAire.getText().isEmpty();
+                camposCompletos &= tieneABS.getSelectedToggle() != null;
+                break;
+
+            case "buses":
+                camposCompletos &= !txtMarca.getText().isEmpty();
+                camposCompletos &= esNuevo.getSelectedToggle() != null;
+                camposCompletos &= !txtModelo.getText().isEmpty();
+                camposCompletos &= cbxCambio.getValue() != null;
+                camposCompletos &= !txtVelocidad.getText().isEmpty();
+                camposCompletos &= !txtCilindraje.getText().isEmpty();
+                camposCompletos &= !txtNumPasajeros.getText().isEmpty();
+                camposCompletos &= !txtPuertas.getText().isEmpty();
+                camposCompletos &= !txtCapacidadMaletero.getText().isEmpty();
+                camposCompletos &= tieneAireAcon.getSelectedToggle() != null;
+                camposCompletos &= tieneCamara.getSelectedToggle() != null;
+                camposCompletos &= !txtNumBolsasAire.getText().isEmpty();
+                camposCompletos &= tieneABS.getSelectedToggle() != null;
+                camposCompletos &= !txtNumEjes.getText().isEmpty();
+                break;
+            
+            case "Camiones":
+                camposCompletos &= !txtMarca.getText().isEmpty();
+                camposCompletos &= esNuevo.getSelectedToggle() != null;
+                camposCompletos &= !txtModelo.getText().isEmpty();
+                camposCompletos &= cbxCambio.getValue() != null;
+                camposCompletos &= !txtVelocidad.getText().isEmpty();
+                camposCompletos &= !txtCilindraje.getText().isEmpty();
+                camposCompletos &= !txtCapacidadCarga.getText().isEmpty();
+                camposCompletos &= tieneAireAcon.getSelectedToggle() != null;
+                camposCompletos &= tieneFrenosAire.getSelectedToggle() != null;
+                camposCompletos &= tieneABS1Cam.getSelectedToggle() != null;
+                camposCompletos &= !txtNumEjesCam.getText().isEmpty();
+                break;
+
+            default:
+                camposCompletos = false;
+                break;
+
+        }
+        return camposCompletos;}
+
+
+//----------------------- Message methods ----------------
+
+public void mensajeInformacion(String title, String context){
+     Alert alert = new Alert(Alert.AlertType.INFORMATION);
+     alert.setTitle(title);
+     alert.setHeaderText(null);
+     alert.setContentText(context);
+     alert.showAndWait();
+
+}
+
+public void mensajeError(String title, String context){
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setTitle(title);
+    alert.setHeaderText(" Error!!");
+    alert.setContentText(context);
+    alert.showAndWait();   
+}
 }
